70a745e3cb2a4f4ea457d9f45749ef403adef71d
Your Name <you@example.com>
2025-02-17 16:46:08 +0100

    fixing code incorrections and implementing splitt= =ing dataset and other stuff

--- main.py
+++ main.py
@@ -37,19 +37,25 @@ for text in data:
     x = x[1:] + [stoi[chr]]
   #print('-------')
 
-X, Y = torch.tensor(X), torch.tensor(Y)
+# split ds
+a = int(0.8 * len(X)) # 80% train 
+b = int(0.9 * len(X)) # 10% val and test 
+
+Xtr, Ytr = torch.tensor(X[:a]), torch.tensor(Y[:a])        # train ds
+Xval, Yval = torch.tensor(X[a:b]), torch.tensor(Y[a:b])    # validation  ds
+Xte, Yte = torch.tensor(X[b:]), torch.tensor(Y[b:])        # test ds
 
 # create params
-emb_dim = 2
-fan_in, fan_out = block_size * emb_dim, 100
+emb_dim = 20
+fan_in, fan_out = block_size * emb_dim, 400
 
 C = torch.randn((vocab_size, emb_dim))
 
 W1 = torch.randn((fan_in, fan_out))
 b1 = torch.randn((fan_out))
 
-W2 = torch.randn((fan_out, vocab_size)) * 0.1 # * 0.1 for init W2 in some way that the loss does not go to inf
-b2 = torch.randn((vocab_size)) * 0.1 # * 0.1 for init W2 in some way that the loss does not go to inf
+W2 = torch.randn((fan_out, vocab_size)) * 0.01 # * 0.1 for init W2 in some way that the loss does not go to inf
+b2 = torch.randn((vocab_size)) * 0.01 # * 0.1 for init W2 in some way that the loss does not go to inf
 
 params = [C, W1, b1, W2, b2]
 num_params = 0
@@ -57,28 +63,34 @@ num_params = 0
 # count tot number of params and # activate grad tracking
 for p in params:
   p.requires_grad = True
-  
+
   num_params += p.numel()
 
+# keep track of loss
+lsteps = []
+lloss = []
+
 # train network
-batch_size = 32
-steps = 10000
+batch_size = 64
+steps = 20000
 
 for i in range(steps):
   # create network input
-  x = C[X].view(-1, fan_in)
-  idxs = torch.randint(0, X.shape[0], size=(batch_size,))
-  x_emb = x[idxs]
+  idxs = torch.randint(0, Xtr.shape[0], size=(batch_size,))
+  x_emb = C[Xtr[idxs]].view(-1, fan_in)
 
   # forward pass
   z = x_emb @ W1 + b1
   a = torch.tanh(z)
 
-  logits = z @ W2 + b2
+  logits = a @ W2 + b2
   probs = nn.functional.softmax(logits, dim=1)
 
   # calc loss
-  loss = -probs[torch.arange(batch_size), Y[torch.arange(batch_size)]].log().mean()
+  loss = nn.functional.cross_entropy(logits, Ytr[idxs])
+
+  lsteps.append(i)
+  lloss.append(loss.item())
 
   # set grat to None
   for p in params:
@@ -88,14 +100,40 @@ for i in range(steps):
   loss.backward()
 
   # gradient steps
-  lr = 0.1 if i < 5000 else 0.01
+  lr = 0.01
 
   for p in params:
-    p.data += lr * -p.grad 
-  
+    p.data += lr * -p.grad
+
   # stats
   if i % (steps * 0.1) == 0:
     print(f'step {i} / {steps} | loss: {loss.item()}')
+  
+print(f'step {steps} / {steps} | loss: {loss.item()}')
+
+# test on train set
+x_emb = C[Xtr].view(-1, fan_in)
+
+z = x_emb @ W1 + b1
+a = torch.tanh(z)
+
+logits = a @ W2 + b2
+
+loss = nn.functional.cross_entropy(logits, Ytr)
+
+print(f'train loss: {loss.item()}')
+
+# test on val set
+x_emb = C[Xval].view(-1, fan_in)
+
+z = x_emb @ W1 + b1
+a = torch.tanh(z)
+
+logits = a @ W2 + b2
+
+loss = nn.functional.cross_entropy(logits, Yval)
+
+print(f'test loss: {loss.item()}')
 
 # inference (generate passwords)
 for i in range(20):
@@ -113,12 +151,12 @@ for i in range(20):
       z = x_emb @ W1 + b1
       a = torch.tanh(z)
 
-      logits = z @ W2 + b2
+      logits = a @ W2 + b2
       probs = nn.functional.softmax(logits, dim=1)
 
       # idx to chr
       idx = torch.multinomial(probs, num_samples=1).item()
-    
+
       pwd += itos[idx]
 
       x = x[1:] + [idx]
