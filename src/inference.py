correct most of the errors (not implementing batch normalization)

src/inference.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch
 
 from hyperparameters import hyperparams
@@ -9,7 +10,25 @@ from train import train
 block_size = hyperparams['block size']
 
 # network
-train()
+lossi, probs, sprobs = train()
+
+'''plt.plot(torch.tensor(lossi).view(-1, 1000).mean(1))
+plt.show()'''
+
+# Create a figure with 1 row and 2 columns for side-by-side plots
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))  # (1 row, 2 columns)
+
+# Plot the first histogram on the first axis
+axes[0].hist(sprobs[34].detach(), bins=50)
+axes[0].set_title('Histogram of sprobs')
+
+# Plot the second histogram on the second axis
+axes[1].hist(probs[34].detach(), bins=50)
+axes[1].set_title('Histogram of probs')
+
+# Show the plots
+plt.tight_layout()  # Adjust spacing between subplots
+plt.show()
 
 net = Network(load=True)
 

src/network.py
@@ -32,11 +32,11 @@ class Network:
     layers = [self.fan_in] + [fan_out] * num_layers + [vocab_size]
 
     # embedding tensor
-    self.C = torch.randn(size=(vocab_size, emb_dim), requires_grad=True)
+    self.C = nn.Parameter(torch.randn(size=(vocab_size, emb_dim)))
 
     # weights (avoid vanishing gradient and high loss at the start using kaiming init)
     self.w = nn.ParameterList([
-            nn.Parameter(torch.randn(_in, _out) * (5/3) / (self.fan_in ** 0.5))
+            nn.Parameter(torch.randn(_in, _out) * ((5/3) / (_in ** 0.5)))
             for _in, _out in zip(layers, layers[1:])
     ])
     
@@ -47,15 +47,15 @@ class Network:
     ])
 
     # batch normalization
-    self.bngain = torch.ones(size=(1, fan_out), requires_grad=True)
-    self.bnbias = torch.zeros(size=(1, fan_out), requires_grad=True)
+    self.bngain = nn.Parameter(torch.ones(size=(1, fan_out)))
+    self.bnbias = nn.Parameter(torch.zeros(size=(1, fan_out)))
 
     self.bnmean_running = torch.ones((1, fan_out))
     self.bnstd_running = torch.zeros((1, fan_out))
 
-    self.params = {'C': self.C, 'w': self.w, 'b': self.b, 'bngain': self.bngain, 'bnbias': self.bnbias}
+    self.params = {'C': self.C, 'w': self.w, 'b': self.b} # 'bngain': self.bngain, 'bnbias': self.bnbias}
 
-    self.num_params = self.C.numel() + sum([w.numel() for w in self.w]) + sum([b.numel() for b in self.b]) + self.bngain.numel() + self.bnbias.numel()
+    self.num_params = self.C.numel() + sum(w.numel() for w in self.w) + sum(b.numel() for b in self.b) + self.bngain.numel() + self.bnbias.numel()
 
   def forward(self, x):
     '''
@@ -63,29 +63,29 @@ class Network:
     '''
 
     for w, b in zip(self.w, self.b):
-        z = x @ w + b 
+      z = x @ w + b 
        
-        # batch normalization
-        if self.train:
-          if self.bngain.shape[-1] == z.shape[-1]: # skip batch normalization after the last layer
-              bnmeani = z.mean(0, keepdim=True)
-              bnstdi = z.std(0, keepdim=True)
+      # batch normalization
+      '''
+      if self.train:
+        if self.bngain.shape[-1] == z.shape[-1]: # skip batch normalization after the last layer
+            bnmeani = z.mean(0, keepdim=True)
+            bnstdi = z.std(0, keepdim=True)
 
-              z = self.bngain * (z - bnmeani) / bnstdi + self.bnbias 
-        else:
-          if self.bngain.shape[-1] == z.shape[-1]:
-            z = self.bngain * (z - self.bnmean_running) / self.bnstd_running + self.bnbias 
-   
-        a = torch.tanh(z)
+            xhat = (z - bnmeani) / torch.sqrt(bnstdi + 1e-5)
+            z = self.bngain * xhat + self.bnbias 
+            
+        with torch.no_grad():
+          self.sbnmean_running = 0.999 * self.bnmean_running + 0.001 * bnmeani
+          self.bnstd_running = 0.999 * self.bnstd_running + 0.001 * bnstdi
+      else:
+        if self.bngain.shape[-1] == z.shape[-1]:
+          z = self.bngain * (z - self.bnmean_running) / (self.bnstd_running + 1e-5) + self.bnbias 
+      '''
+      a = torch.tanh(z)
+      x = a
 
-        if self.train:
-          with torch.no_grad():
-              self.sbnmean_running = 0.999 * self.bnmean_running + 0.001 * bnmeani
-              self.bnstd_running = 0.999 * self.bnstd_running + 0.001 * bnstdi
-        
-        x = a
-
-    probs = nn.functional.softmax(x, dim=1) # (B, V), V = vocab_size
+    probs = nn.functional.softmax(z, dim=1) # (B, V), V = vocab_size
 
     return probs
 

src/train.py
@@ -5,9 +5,14 @@ from network_ds import Xtr, Ytr
 from hyperparameters import hyperparams
 from network import Network
 
+lossi = []
+
 def train():
   net = Network()
-  
+
+  # network data
+  print(f'Number of parameters: {net.num_params}')
+
   # hyperparameters
   steps = hyperparams['steps']
   batch_size = hyperparams['batch size']
@@ -20,6 +25,9 @@ def train():
 
     # forward pass
     probs = net.forward(x)
+
+    if i == 0:
+      sprobs = probs
 
     # calc loss
     loss = -probs[torch.arange(batch_size), Ytr[idxs]].log().mean()
@@ -36,27 +44,24 @@ def train():
     loss.backward()
 
     # gradient steps
-    lr = 0.1 if i < 100000 else 0.01
+    lr = 0.1 if i < (steps // 2) else 0.01
 
-    with torch.no_grad():
-      for p in net.params.values():
-          if isinstance(p, nn.ParameterList):
-            for j in p:
-                  if j.grad is not None:
-                      j -= lr * j.grad
-
-                      j.grad.zero_()
-          else:
-              if p.grad is not None:
-                  p -= lr * p.grad
-
-                  p.grad.zero_()
+    for p in net.params.values():
+      if isinstance(p, nn.ParameterList):
+        for j in p:
+          j.data += lr * -j.grad
+      else:
+        p.data += lr * -p.grad
 
     # stats
     if i % (steps * 0.1) == 0:
       print(f'step {i} / {steps} | loss: {loss.item():.3f}')
+    
+    lossi.append(loss.log10().item())
 
   print(f'step {steps} / {steps} | loss: {loss.item():.3f}')
 
   # save network params
   net.save()
+
+  return lossi, probs, sprobs